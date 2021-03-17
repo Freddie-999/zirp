643c0d619a9ea0d5f3df4c218b5a9a9cc9c67bc3
Freddie <frederick.abalo@icloud.com>
2021-03-17 17:32:18 +0000

    refactored the code and also attempted to add a guard, if __name == __main__

--- ir.py
+++ ir.py
@@ -7,7 +7,9 @@ import json
 
 
 def main():
+
     def get_coord():
+
         """get coordinates from user to json """ 
         while True:
             try:
@@ -17,6 +19,7 @@ def main():
                 print('check your input')   
         
     def coord_json():
+
         """converts coord tuple to json"""
         coordinates_dictionary = {
         "type": "Feature",
@@ -47,40 +50,25 @@ def main():
         return districtboundary_projected
 
 
-    def read_towns():
-        """get coordinates of towns"""
-        df = pd.read_csv('/Users/freddie/Downloads/jira_data.csv')
-        points = df.apply(lambda row: Point(row.Lon, row.Lat), axis=1)
-        towns_gdf = gpd.GeoDataFrame(df, geometry=points)
-        proj = towns_gdf.set_crs(epsg=3857)
-        proj.set_index('Summary', inplace=True)
-        proj.to_csv("/Users/freddie/Downloads/new.csv", index=True, mode='w')
-        return proj
 
-
-    def get_nearest_town():
-        """find the nearest town to incident"""
-        pass
-
-
-    def get_info():
-        """return the info for the nearest town"""
-        pass
-
-
-
+    def get_towns():
+        """read the districts to gpd""" 
+        towns = gpd.read_file('/Users/freddie/Downloads/zz/zirp.shp')
+        towns_projected = towns.to_crs(epsg=3857)
+        print("\n\n\n these are the towns...")
+        print(towns_projected)
+        return towns_projected
+        
 
     def get_geoprocess():
 
-
         #spatial analysis
-        buffer = mypoint['geometry'].buffer(distance = 1200)
+        buffer = mypoint['geometry'].buffer(distance = 1200)        
         buffer_df = gpd.GeoDataFrame(gpd.GeoSeries(buffer))
         buffer_df = buffer_df.rename(columns={0:'geometry'}).set_geometry('geometry')
-        print("this is buffer 1.....\n")
         print(buffer_df)
-        return buffer_df
         buffer_df.to_file(r"/Users/freddie/test2.shp")
+
 
         join = gpd.sjoin(mydistrict, buffer_df, how="inner", op="intersects")
         print('this is a join....\n')
@@ -89,41 +77,48 @@ def main():
 
         print('preparing another buffer\n')
 
-        buffer2 = mypoint['geometry'].buffer(distance = 50000)
-        buffer2_df = gpd.GeoDataFrame(gpd.GeoSeries(buffer))
-        buffer2_df = buffer2_df.rename(columns={0:'geometry'}).set_geometry('geometry')
-        print("this is buffer 2.......\n")
-        print(buffer2_df)
-        buffer2.to_file(r"/Users/freddie/luck.shp")
+        return buffer_df
 
-        shw = mytowns['geometry'].within(buffer2_df)
-        shw.to_csv("tryme.csv", index=True, mode="w")
-        print(shw)
+    def buffer_point():
+        buffer_new = mypoint['geometry'].buffer(distance = 5000)        
+        buffer_new_df = gpd.GeoDataFrame(gpd.GeoSeries(buffer_new))
+        buffer_new_df = buffer_new_df.rename(columns={0:'geometry'}).set_geometry('geometry')
+        print(buffer_new_df)
+        return buffer_new_df
+        
+
+    def get_aoi_towns():
+        """get towns that are with the 3000m buffer"""
+        towns_in_aoi = gpd.overlay(alltowns, aoi_buffer, how = 'intersection')
+        print("these are the towns we really want to look at")
+        print(towns_in_aoi)
+        return towns_in_aoi
+
 
     def add_basemap():
         print("Drawing map, please wait...")
         #plotting to map
         plt.rcParams['figure.figsize'] = [10, 10] #this sets the size of the figure
         fig, myax = plt.subplots()
-        #mydistrict.plot(ax=myax, ec='gray', alpha=0.2, column='Name')
-        get_geoprocess().plot(ax=myax, ec='gray', alpha=0.2, edgecolor='black')
         mypoint.plot(ax=myax, color='red')
-        #join.plot(ax=myax, ec='gray', alpha=0.2, edgecolor='black')
-        #mytowns.plot(ax=myax, color='green')
-        print("trying to add points")
-        #read_towns().plot(ax=myax)
+        get_geoprocess().plot(ax=myax, ec='gray', alpha=0.2, edgecolor='black')
+        my_aoi_towns.plot(ax=myax, color='green')
         
         src_basemap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         ctx.add_basemap(ax=myax, source=src_basemap, alpha=0.6, zorder=8)
         plt.show()
 
 
+
     coordinates = get_coord()
     coord_json()                   
     mypoint = read_point()
     mydistrict = read_shape_file()
-    mytowns = read_towns()
+    alltowns = get_towns()
     get_geoprocess()
+    aoi_buffer = buffer_point()
+    my_aoi_towns = get_aoi_towns()
+
     add_basemap() 
 
 if __name__ == '__main__':
